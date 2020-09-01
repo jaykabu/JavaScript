b75f430fc1578cc5b996d1dc8e4bf20b1045184a
jaykabu <59605424+jaykabu@users.noreply.github.com>
2020-09-01 14:18:03 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,66 @@
+let age = 23;
+const vari = 30;
+const doesDrive = true;
+
+
+// if (age == 19){
+//     console.log(age);
+// }
+// else if (age == 23){
+//     console.log("age is 23");
+// }
+// else {
+//     console.log("age not defined");
+// }
+// // console.log(age);
+//
+// if (typeof vari !== 'undefined'){
+//     console.log('vari is defined');
+// }
+// else {
+//     console.log('vari is not defined');
+// }
+
+// if(doesDrive && age > 18 ){
+//     console.log("you can drive")
+// }
+// else {
+//     console.log("you cannot drive");
+// }
+
+// if (age>18 || doesDrive ||(typeof vari !== "undefined")){
+//     console.log("right");
+//
+// }
+// else {
+//     console.log("wrong")
+// };
+
+// console.log(age==23? 'age is 23':'age is not 23');
+
+// switch (age) {
+//     case 18:
+//         console.log('you are 18');
+//         break;
+//     case 45:
+//         console.log('you are 45');
+//         break;
+//     case 44:
+//         console.log('you are 44');
+//         break;
+//     default:
+//         console.log('you are unknown age');
+//         break;
+// }
+
+
+
+const display = true;
+let age1 = 16;
+
+if (typeof display !== 'undefined' && (age1>18) ){
+    console.log("display is on")
+}
+else {
+    console.log("display is off")
+}
