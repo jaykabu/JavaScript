520640953682ca079bc9df234a3810d31078df97
jaykabu <59605424+jaykabu@users.noreply.github.com>
2020-09-01 16:44:11 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,64 @@
+//For Loop
+
+// for (let i=98;i<100;i++)
+// {
+//     console.log(i)
+// }
+
+//while loop
+
+// let j = 0;
+// while (j<10){
+//     console.log(j+1)
+//     j++
+// }
+
+//Do While Loop
+
+// let k = 0;
+// do {
+//     console.log(k);
+//     if (k===5){
+//         break;
+//     }
+//     k++;
+// }while (k<10);
+
+
+
+// a = 0;
+// do {
+//     if (a===3){
+//         a++;
+//         continue;
+//     }
+//     console.log(a);
+//     a++;
+// }while (a<10);
+
+
+// let arr = [2,4,6,8,10,12];
+//
+// for (let i = 0; i<arr.length;i++){
+//     const element = arr[i];
+//     console.log(element)
+// };
+
+
+// arr.forEach(function (element,index,array) {
+//     console.log(element,index,array);
+// });
+//
+//
+// let obj = {
+//     name:"jay",
+//     age:23,
+//     hobby:"cricket"
+// };
+//
+// for (let key in obj){
+//     console.log(`the ${key} of object is ${obj[key]}`);
+// }
+
+
+
