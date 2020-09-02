b12d3b9283ab89cf0250ab5f2739963dbc81bd0b
jaykabu <59605424+jaykabu@users.noreply.github.com>
2020-09-02 16:17:29 +0530

    Add files via upload

---
+++ ManipulatingWebsites.js
@@ -0,0 +1,19 @@
+let a = window.document;
+
+// alert("jay");
+// a = prompt("type of your name");
+// a= confirm("delete the page?");
+// a=window.innerHeight;
+a = window.innerWidth;
+a = scrollY;
+a= location.toString();
+console.log(a);
+
+let b = window;
+
+// b = prompt("type name");
+// b = confirm("delete page");
+b = innerHeight;
+b= innerWidth;
+b = location.toString();
+console.log(b);
